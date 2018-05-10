fa223b5f00bd2b7cd4108765a19f7d16c878d40d
Zachary Blackwood <zachary.blackwood@spensatech.com>
2018-05-10 12:17:25 -0400

    First commit

---
+++ lists/tests.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+
+class HomePageTest(TestCase):
+
+	def test_home_page(self):
+		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
